d307bbc8cc94002114bbcfe390726b6a726d2815
Spyke Lionel <59184392+spykelionel@users.noreply.github.com>
2023-10-19 23:46:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import csv
 import requests
 
-# Open the CSV file
 with open('Task 2 - Intern.csv', 'r') as file:
     reader = csv.reader(file)
-    next(reader)  # Skip the header row if present
+    next(reader)
 
-    # Process each row in the CSV file
     for row in reader:
-        url = row[0]  # Assuming the URL is in the first column
-
+        url = row[0]  
         try:
             response = requests.get(url)
             status_code = response.status_code
