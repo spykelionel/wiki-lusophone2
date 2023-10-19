d3cf247f26c3478ffab709791a9b7843d730d262
spykelionel <ndilionel6@gmail.com>
2023-10-19 23:52:51 +0100

    switch to OOP

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import csv
 import requests
 
-with open('Task 2 - Intern.csv', 'r') as file:
-    reader = csv.reader(file)
-    next(reader)
+class URLChecker:
+    def __init__(self, file_path):
+        self.file_path = file_path
 
-    for row in reader:
-        url = row[0]  
+    def check_urls(self):
+        with open(self.file_path, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            for row in reader:
+                url = row[0]
+                self.check_url(url)
+
+    def check_url(self, url):
         try:
             response = requests.get(url)
             status_code = response.status_code
             print(f"({status_code}) {url}")
         except requests.exceptions.RequestException:
             print(f"(Error) {url}")
+
+url_checker = URLChecker('Task 2 - Intern.csv')
+url_checker.check_urls()
